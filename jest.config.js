f0be11e76d733496260c4dbe849e439bc2d864b3
EdwinGuerreroCortez <20221030@uthh.edu.mx>
2025-10-29 11:16:16 -0600

    feat: eliminar configuración de Babel y actualizar transformaciones en Jest para TypeScript

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
-// jest.config.js
 export default {
     testEnvironment: "node",
+
     testMatch: [
         "**/src/__tests__/**/*.test.js"
     ],
+
     transform: {
-        "^.+\\.(js|jsx)$": "babel-jest"
+        "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
     }
 };
